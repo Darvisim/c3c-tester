80a3121087d6c1de0787f005c34d9f68bf967782
Darvis <dsouzavishal685@gmail.com>
2026-03-11 08:11:26 +0530

    Enhance sync.sh with latest commits made to upstream branch if any

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -6,15 +6,45 @@ CLONE_DIR="./c3c"
 
 echo "::group::Syncing c3c repository"
 
-if [ -d "$CLONE_DIR" ]; then
-    echo "Repository exists. Pulling latest changes..."
+if [ -d "$CLONE_DIR/.git" ]; then
+    echo "Repository exists. Checking for updates..."
+
     cd "$CLONE_DIR"
-    git fetch origin
-    git reset --hard origin/master
+
+    OLD_HASH=$(git rev-parse --short HEAD)
+
+    git fetch origin master
+
+    NEW_HASH=$(git rev-parse --short origin/master)
+
+    if [ "$OLD_HASH" = "$NEW_HASH" ]; then
+        echo "Already up to date ($OLD_HASH)"
+    else
+        echo "Updates detected:"
+        echo
+
+        # Show commits merged into upstream/master since last sync
+        git log --pretty=format:'%h -> %h : %s' HEAD..origin/master
+
+        echo
+        echo "Updating repository..."
+
+        git reset --hard origin/master
+    fi
+
     cd ..
+
 else
     echo "Cloning c3c repository..."
     git clone "$REPO_URL" "$CLONE_DIR"
+
+    cd "$CLONE_DIR"
+
+    NEW_HASH=$(git rev-parse --short HEAD)
+
+    echo "Initial clone at commit $NEW_HASH"
+
+    cd ..
 fi
 
 echo "::endgroup::"
