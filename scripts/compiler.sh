299c3b94270279552825942ad87ff32db9ce8e45
Darvis <dsouzavishal685@gmail.com>
2026-03-11 08:25:18 +0530

    Fix cmake build commands in compiler.sh

--- scripts/compiler.sh
+++ scripts/compiler.sh
@@ -6,14 +6,14 @@ BUILD_DIR="$C3_DIR/build"
 
 echo "::group::Building C3 Compiler"
 
-mkdir -p "$BUILD_DIR"
-cd "$BUILD_DIR"
+cmake -S "$C3_DIR" -B "$BUILD_DIR" \
+    -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DC3_LLVM_FETCH=ON
 
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DC3_LLVM_FETCH=ON
-cmake --build build
+cmake --build "$BUILD_DIR" --parallel
 
 echo "Verifying c3c build..."
 "$BUILD_DIR/c3c" --version
 
-cd ../..
 echo "::endgroup::"
