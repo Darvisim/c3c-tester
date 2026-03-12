e192a9c57be6db2b12e44d7dba6fefa5dab578d1
Darvis <dsouzavishal685@gmail.com>
2026-03-13 00:15:17 +0530

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -10,13 +10,18 @@ if [ -d "$DIR/.git" ]; then
     echo "Updating existing repo"
     git -C "$DIR" fetch origin
     git -C "$DIR" reset --hard origin/HEAD
+
+elif [ -d "$DIR" ]; then
+    echo "Directory exists but is not a git repo. Recreating..."
+    rm -rf "$DIR"
+    git clone --depth 1 "$REPO_URL" "$DIR"
+
 else
     echo "Cloning $REPO_URL"
     git clone --depth 1 "$REPO_URL" "$DIR"
 fi
 
 COMMIT=$(git -C "$DIR" rev-parse --short HEAD)
-
 echo "$DIR commit: $COMMIT"
 
 echo "::endgroup::"
