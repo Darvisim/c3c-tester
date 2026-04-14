bc9df3df4a8d6be5b98dcdd47266004b64b93d38
Darvis <dsouzavishal685@gmail.com>
2026-04-14 15:01:04 +0530

    Restore main.yml

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,7 @@ log_info "Setting up dependencies for $PLATFORM..."
 case "$PLATFORM" in
     Linux)   check_deps cmake ninja curl || { sudo apt-get update; sudo apt-get install -y cmake ninja-build build-essential curl; } ;;
     macOS)   check_deps cmake ninja || brew install cmake ninja ;;
-    Windows) check_deps cmake ninja || choco install cmake ninja -y ;;
+    Windows) check_deps cmake ninja || choco install cmake ninja zstd -y ;;
     *)       log_error "Unsupported platform: $PLATFORM"; exit 1 ;;
 esac
 
